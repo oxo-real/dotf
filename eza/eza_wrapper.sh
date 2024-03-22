411c50edfff6d5ae1d6ce7ecd56286a809795dfd
oxo <2oxo@protonmail.com>
2024-03-22 18:09:06 +0100

    20240322_180905 (update_mon)

--- eza/eza_wrapper.sh
+++ eza/eza_wrapper.sh
@@ -1,16 +1,65 @@
 #! /usr/bin/env sh
 
-# [Now moving from exa to eza fork. Wrapper script to give it nearly identical switches and appearance to ls. Also automatically adds --git switch when in a git repository. · GitHub]
-# (https://gist.github.com/eggbean/74db77c4f6404dd1f975bd6f048b86f8)
+###
+###   ___ ______ _  __      ___ __ __ _ _ __  _ __   ___ _ __
+###  / _ \_  / _` | \ \ /\ / / '__/ _` | '_ \| '_ \ / _ \ '__|
+### |  __// / (_| |  \ V  V /| | | (_| | |_) | |_) |  __/ |
+###  \___/___\__,_|___\_/\_/ |_|  \__,_| .__/| .__/ \___|_|
+###              |_____|               |_|   |_|
+###
+###  # # # # # #
+###       #
+###  # # # # # #
+###
 
 : '
-# add to alia
+eza_wrapper.sh
+translate ls flags to eza
+copyright (c) 2023 - 2024  |  oxo
+
+GNU GPLv3 GENERAL PUBLIC LICENSE
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+https://www.gnu.org/licenses/gpl-3.0.txt
+
+@oxo@qoto.org
+
+
+# dependencies
+add to alia:
+
 if command -v eza >/dev/null; then
+
     alias ls="$XDG_CONFIG_HOME/eza/eza-wrapper.sh"
+
 else
+
     alias ls="command ls $LS_OPTIONS"
+
 fi
+
+# usage
+  n/a
+
+# examples
+  n/a
+
+# notes
+[Now moving from exa to eza fork. Wrapper script to give it nearly identical switches and appearance to ls. Also automatically adds --git switch when in a git repository. · GitHub]
+(https://gist.github.com/eggbean/74db77c4f6404dd1f975bd6f048b86f8)
+
 # '
+
 
 ## Change following to '0' for output to be like ls and '1' for eza features
 # Don't list implied . and .. by default with -a
--- nvim/init.vim
+++ nvim/init.vim
@@ -1,22 +1,50 @@
 "
-"#
-"##
+"
 "###        _
 "### __   _(_)_ __ ___  _ __ ___
 "### \ \ / / | '_ ` _ \| '__/ __|
 "###  \ V /| | | | | | | | | (__
 "### (_)_/ |_|_| |_| |_|_|  \___|
-"###  _ _|_ _ ._    _  _
-"### (_\/|_(_)|_)\/(_|(/_
-"###   /      |  /  _|
 "###
-"### vim runcom configuration
 "###
-"### (c) 2017 - 2022 cytopyge
+"### # # # # # #
+"###      #
+"### # # # # # #
 "###
-"##
-"#
-"
+
+": '
+"init.vim
+"vim runcom configuration
+"copyright (c) 2017 - 2022  |  oxo
+
+"GNU GPLv3 GENERAL PUBLIC LICENSE
+"This program is free software: you can redistribute it and/or modify
+"it under the terms of the GNU General Public License as published by
+"the Free Software Foundation, either version 3 of the License, or
+"(at your option) any later version.
+
+"This program is distributed in the hope that it will be useful,
+"but WITHOUT ANY WARRANTY; without even the implied warranty of
+"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+"GNU General Public License for more details.
+
+"You should have received a copy of the GNU General Public License
+"along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"https://www.gnu.org/licenses/gpl-3.0.txt
+
+"@oxo@qoto.org
+
+
+"# dependencies
+"  n/a
+
+"# usage
+"  n/a
+
+"# examples
+"  n/a
+
+"# '
 
 
 "# loose beginnings
